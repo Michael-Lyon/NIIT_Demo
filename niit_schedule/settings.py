3ee85f70993beb6ec33df4c11c117b3736e7a4bb
Michael Lyon <michaellyon@localhost.localdomain>
2023-05-21 15:57:47 +0100

    first commit3

--- niit_schedule/settings.py
+++ niit_schedule/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+import os
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -72,6 +72,12 @@ CORS_ALLOW_HEADERS = [
 CSRF_TRUSTED_ORIGINS = ['https://niit-demo-timetable.up.railway.app', "http://localhost:8000"]
 
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 TEMPLATES = [
     {
