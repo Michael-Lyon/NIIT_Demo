e205f3e8322ff6636d2b13142d80d0d4c4c95d19
Michael Lyon <michaellyon@localhost.localdomain>
2023-05-22 15:09:03 +0100

    Added more details

--- core/serializers.py
+++ core/serializers.py
@@ -15,6 +15,19 @@ class ClassSerializer(serializers.ModelSerializer):
 
 
 class ScheduleSerializer(serializers.ModelSerializer):
+    class_name = serializers.SerializerMethodField()
+    teacher = serializers.SerializerMethodField()
+    duration = serializers.SerializerMethodField()
+
     class Meta:
         model = Schedule
-        fields = '__all__'
+        fields = ['day', 'duration', 'class_name', 'teacher']
+
+    def get_class_name(self, obj):
+        return obj.class_obj.name
+
+    def get_teacher(self, obj):
+        return obj.class_obj.teacher
+
+    def get_duration(self, obj):
+        return obj.class_obj.duration
